05cc61e6174a61792a275feaec5f1aa9b3c4830a
Francisco Analista Programador <aprog1978@yahoo.com>
2020-05-29 15:11:40 -0300

    Curso de PHP #02 - Constantes e Variáveis

--- aula02/index.php
+++ aula02/index.php
@@ -20,15 +20,37 @@ and open the template in the editor.
         $vnum2 = 20.5; //flout
         $soma =0 ;
         
+        //$soma = 10 + 20;
+        $soma = $vnum + $vnum2;
+        
+        //Constante;
+        define("cnome", "Francisco");
+        define("cpi",3.1415);
+        define("cnomeProgramador","Francisco Analista Programador");
+        define("cver", PHP_VERSION);
+        define("cdir", __DIR__);
+        define("cfile", __FILE__);
+                
+        
         echo "nome: $vnome"
                 . "<br>numero: $vnum"
                 . "<br>numero2: $vnum2"
                 . "<br>soma: $soma"; 
         
         $vnome = "Campos";
-        echo "<hr>";
+        echo "<hr/>";
         echo "nome: ". $vnome;
         
+        echo "<hr/>";
+        echo "Constante: ". cnome . "<br/>"
+                . "Pi:" . cpi . "<br/>"
+                . "Programador:".cnomeProgramador. "<br/>"
+                . "Const. Predeterminada nome arquivo:".cfile."<br/>"
+                ."Versao PHP". cver. "<br/>"
+                . "Dir:".cdir."<br/>"
+                . "Versao SO:".PHP_OS."<br/>"
+                . "num line:".__LINE__."<br/>";
+       
         ?>
     </body>
 </html>
