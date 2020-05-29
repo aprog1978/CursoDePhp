9bb2bd08c296b5301115b6293011824d5c1ebf34
Francisco Analista Programador <aprog1978@yahoo.com>
2020-05-29 00:18:02 -0300

    aula 01

---
+++ aula01/aula1.php
@@ -0,0 +1,12 @@
+<?php
+
+// Comentario simples em php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "<font color=#ff0000>Primeira linha me PHP</font><br />";
+echo "Curso de PHP";
+?>
